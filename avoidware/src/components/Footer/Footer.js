3759a230f1779d38c385e3dbaae153bfb752b485
Nicolas Tobar <tobar.n@husky.neu.edu>
2020-11-21 00:18:43 -0500

    working on footer

--- avoidware/src/components/Footer/Footer.js
+++ avoidware/src/components/Footer/Footer.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React from "react";
 import "./Footer.css";
 
-function Footer {
-
+function Footer() {
   return (
     <div className="footer">
-    <div className="container">
-    <div className="row">
-    
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <ul className="list_unstyled">
+              <li>what is this</li>
+              <li>moscow russia</li>
+              <li>123 mosword csreart</li>
+              <li>point pointen</li>
+            </ul>
+            <div className="column">
+              <h4>stuff</h4>
+              <li>what is this</li>
+              <li>moscow russia</li>
+              <li>123 mt</li>
+              <li>point </li>
+              <ul className="list_unstyled">
+                <li>what is this</li>
+              </ul>
+            </div>
+          </div>
+          <div className="row">
+            <p className="col-smol">
+              &copy;[new Date().getFullYear()] Thick memes inc |rights reserved
+              | terms of service |privacy
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-
-
-  )
+  );
 }
+
+export default Footer;
