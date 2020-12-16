cfc1213fbe70f443474865102c995079121a24d7
Nicolas Tobar <tobar.n@husky.neu.edu>
2020-12-16 18:13:19 -0500

    created error component

--- avoidware/src/components/Error/Error.js
+++ avoidware/src/components/Error/Error.js
@@ -5,17 +5,27 @@ import { useStateValue } from "../../StateProvider";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
 function Error() {
-  const [{ error }, dispatch] = useStateValue();
+  const [{ error, url }, dispatch] = useStateValue();
 
   return (
     <div className="error_container">
       <div className="error_title">
-        <h1>There was an Error while scanning the given URL</h1>
+        <div className="warning_icon">
+          <h1>⚠️</h1>
+        </div>
+        <h1>There was an Error while scanning the following URL:</h1>
+        <ArrowDropDownIcon className="arrow_icon" />
+        <p>{url}</p>
       </div>
       <div className="error_desc">
-        <ArrowDropDownIcon className="arrow_icon" />
-        <h1>Error: {JSON.stringify(error.message)}</h1>
-        <h1>Error code from the ZAP API: {JSON.stringify(error.code)}</h1>
+        <div className="error_specific">
+          <h1>Error:</h1>
+          <p> {JSON.stringify(error.message)}</p>
+        </div>
+        <div className="error_specific">
+          <h1>Error code from the ZAP API:</h1>
+          <p>{JSON.stringify(error.code)}</p>
+        </div>
       </div>
     </div>
   );
